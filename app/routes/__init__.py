6f7958ac973907885f15973dd87932fd52637d8a
Sweetsan <coderintheworldhack100@gmail.com>
2026-07-05 23:10:02 +0530

    import user bp in init py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 from app.routes.auth_routes import auth_bp
+from app.routes.user_routes import user_bp
 
 
 def register_blueprints(app):
 
     app.register_blueprint(auth_bp)
+    app.register_blueprint(user_bp)
