07663c055fc19dcf99da752001216568fd6a6c3c
Sweetsan <coderintheworldhack100@gmail.com>
2026-07-05 23:07:52 +0530

    user routes is created

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -4,3 +4,27 @@ from app.controllers import user_controller as ctrl
 
 user_bp = Blueprint("users", __name__, url_prefix="/api/users")
 
+@user_bp.route("", methods=["POST"])
+@roles_required("admin")
+def create_user():
+    return ctrl.create_user()
+
+@user_bp.route("", methods=["GET"])
+@roles_required("admin")
+def get_users():
+    return ctrl.get_users()
+
+@user_bp.route("/<int:user_id>", methods=["GET"])
+@roles_required("admin")
+def get_user(user_id):
+    return ctrl.get_user(user_id)
+
+@user_bp.route("/<int:user_id>", methods=["PUT"])
+@roles_required("admin")
+def update_user(user_id):
+    return ctrl.update_user(user_id)
+
+@user_bp.route("/<int:user_id>", methods=["DELETE"])
+@roles_required("admin")
+def delete_user(user_id):
+    return ctrl.delete_user(user_id)
