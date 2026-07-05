2edabd7251d90a42726ab9986f65001de8235ce1
Sweetsan <coderintheworldhack100@gmail.com>
2026-07-05 22:09:31 +0530

    user model created

---
+++ app/models/user_model.py
@@ -0,0 +1,26 @@
+from app.extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "role_id": self.role_id,
+        }
